bc061ce29657b0d5b500451be4de7b3cb82e3193
Alexandros Papakostopoulos <a.papakostopoulos@gmail.com>
2024-01-21 14:48:04 +0200

    ookla settings

--- ookla/wsgi.py
+++ ookla/wsgi.py
@@ -11,7 +11,18 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-settings_module = 'ookla.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'ookla.settings'
+# settings_module = 'ookla.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'ookla.settings'
+
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
+
+# application = get_wsgi_application()
+
+
+# Check for Azure-specific environment variables
+is_azure = 'WEBSITE_SITE_NAME' in os.environ and 'WEBSITE_INSTANCE_ID' in os.environ
+
+# Set the Django settings module based on the environment
+settings_module = 'ookla.deployment' if is_azure else 'ookla.settings'
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
 
